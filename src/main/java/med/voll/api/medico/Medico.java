f2f92272821bcba11d539f5ff9ed5d0cebb751a2
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2023-12-06 22:38:04 -0300

    Requisições DELETE - Exclusão lógica

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -27,9 +27,11 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -48,5 +50,9 @@ public class Medico {
         if (dados.endereco() != null) {
             this.endereco.atulizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
