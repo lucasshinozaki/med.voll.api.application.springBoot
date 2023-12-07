e5cbe8d54314685e4ec2831fd0c356bdb1ad6328
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2023-12-07 13:26:57 -0300

    Configuração de segurança

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,24 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+}
