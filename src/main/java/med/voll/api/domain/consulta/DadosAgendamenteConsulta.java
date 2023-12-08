ec55210e64a8e4fef2a8e9d0c53202041a079052
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2023-12-08 12:27:48 -0300

    Nova funcionalidade, Agendamento

---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamenteConsulta.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamenteConsulta(
+        Long idMedico,
+        @NotNull
+        Long idPaciente,
+        @NotNull
+        @Future
+        LocalDateTime data) {
+}
