1e28f9005f480db45dda70f0f1cf52f8408236c1
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2023-12-08 00:01:10 -0300

    Adicionando auth0 JWT e gerando tokens JWT

--- src/main/java/med/voll/api/controller/AutentificacaoController.java
+++ src/main/java/med/voll/api/controller/AutentificacaoController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutentificacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutentificacaoController {
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutentificacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
